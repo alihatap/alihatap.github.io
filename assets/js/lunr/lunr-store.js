b1d3a2b301b14ef52eaa8185d3aff78890fc7d7d
alihatap <ali.hatap@gmail.com>
2022-03-23 22:11:27 +0100

    new posts

--- assets/js/lunr/lunr-store.js
+++ assets/js/lunr/lunr-store.js
@@ -4,4 +4,16 @@ var store = [{
         "tags": ["research tools"],
         "url": "/historian's%20toolkit/note-taking/",
         "teaser": "https://webstockreview.net/images/important-clipart-note-taking-2.png"
+      },{
+        "title": "Urban hierarchies of science in mid- to late Victorian England: The literary and philosophical societies in the Midlands",
+        "excerpt":"Abstract of my presentation to be given during the Journées des Jeunes Chercheur.se.s du Centre Alexandre Koyré on 19 May 2022. From the 1780s, the literary and philosophical societies (Lit and Phils) emerged as pioneers of science culture in provincial England. Whilst their high subscription fees secured their privileged status...","categories": ["oral presentations"],
+        "tags": ["abstract"],
+        "url": "/oral%20presentations/abstract-cak/",
+        "teaser": null
+      },{
+        "title": "Science on Local, Region, and National Scales: Geographies of Natural History in the mid- to late Victorian England",
+        "excerpt":"Abstract of my presentation to be given during the Colloquium “Amateurs in Science: a history from below” organised as part of the AmateurS project, 5-7 September 2022. From the 1780s, the literary and philosophical societies (Lit and Phils) emerged as pioneers of science culture in provincial England. Whilst their high...","categories": ["oral presentations"],
+        "tags": ["abstract"],
+        "url": "/oral%20presentations/abstract-mans/",
+        "teaser": null
       }]
